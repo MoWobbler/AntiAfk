1946e76b19e114e34615c6cdb754910088f43eaa
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-10-09 15:09:48 -0400

    Update GetAfkPlayers.java

--- src/net/simpvp/AntiAfk/GetAfkPlayers.java
+++ src/net/simpvp/AntiAfk/GetAfkPlayers.java
@@ -2,6 +2,7 @@ package net.simpvp.AntiAfk;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
@@ -12,31 +13,51 @@ import net.md_5.bungee.api.ChatColor;
 
 public abstract class GetAfkPlayers implements Plugin {
 	
-	// This class labels players as afk and kicks them if tps is bad
+	/* This class labels players as afk and kicks them if tps is bad */
 
+	/* Players are added to playerLastMoveTime when they join or when they do /on */
 	public static Map<Player, Long> playerLastMoveTime = new HashMap<Player, Long>();
 	public static ArrayList<Player> afkPlayers = new ArrayList<>();
 	
 	
-	// This tests for afk players
+	/* This tests for afk players */
     public static void setPlayersAfk() {
         Bukkit.getScheduler().scheduleSyncRepeatingTask(AntiAfk.instance, () -> {
+        	
             for (Map.Entry<Player, Long> entry : playerLastMoveTime.entrySet()) {
             	
-            	// Mark player as afk
+            	/* Mark player as afk */
                 if ((System.currentTimeMillis() - entry.getValue()) > AntiAfk.afk_secs * 1000) {
                     Player p = entry.getKey();
                     afkPlayers.add(p);
-                    p.sendMessage(ChatColor.RED + "You are now afk");
+                    if (AntiAfk.afk_message == true) {
+                        p.sendMessage(ChatColor.RED + "You are now afk");
+                    }
                 }
             }
             for (Player playerToRemove : afkPlayers) {
                 playerLastMoveTime.remove(playerToRemove);
             }
-            // Kick all afk players if tps is lower than the tps set in the config
+            /* Kick all afk players if tps is lower than the tps set in the config */
             if (Double.parseDouble(AntiAfk.gettps.getTPS(0)) < AntiAfk.min_tps && !afkPlayers.isEmpty()) {
-            	KickPlayer.online_check();
+         		Player p;
+         		for (Iterator<?> afkListIterator = AntiAfk.instance.getServer().getOnlinePlayers().iterator(); afkListIterator.hasNext();) {
+         			p = (Player) afkListIterator.next();
+         			if (GetAfkPlayers.isAfk(p)) {
+         				KickPlayer.online_check();
+         			}
+        		}
             }
-        }, 0, 100);
+        }, 0, AntiAfk.scheduler_seconds * 20);
+    }
+    
+    /* return true if player is afk. Used when kicking players in KickPlayer */
+    public static boolean isAfk(Player player) {
+    	if (afkPlayers.contains(player)) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 }
