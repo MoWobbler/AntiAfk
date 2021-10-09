95dc108466260cf4ef0c5577336c9feb20bde2a0
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-10-09 15:10:38 -0400

    Update AntiAfk.java

--- src/net/simpvp/AntiAfk/AntiAfk.java
+++ src/net/simpvp/AntiAfk/AntiAfk.java
@@ -12,7 +12,10 @@ public class AntiAfk extends JavaPlugin {
 	static GetTps gettps = new GetTps();
 	static Double min_tps;
 	static Integer afk_secs;
+	static Integer online_check_seconds;
+	static Integer scheduler_seconds;
 	static List<?> kick_players;
+	static Boolean afk_message;
 	
 	@Override
 	public void onEnable() {
@@ -20,7 +23,10 @@ public class AntiAfk extends JavaPlugin {
 		instance = this;
 		min_tps = AntiAfk.instance.getConfig().getDouble("Minimum_tps");	
 		afk_secs = AntiAfk.instance.getConfig().getInt("Afk_seconds");
+		online_check_seconds = AntiAfk.instance.getConfig().getInt("Online_check_seconds");
+		scheduler_seconds = AntiAfk.instance.getConfig().getInt("Scheduler_check_seconds");
 		kick_players = AntiAfk.instance.getConfig().getStringList("Kick_players").stream().map(UUID::fromString).collect(java.util.stream.Collectors.toList());
+		afk_message = AntiAfk.instance.getConfig().getBoolean("Afk_message");
 		gettps.variables();	
 		GetAfkPlayers.setPlayersAfk();
 		getServer().getPluginManager().registerEvents(new EventListener(), this);
