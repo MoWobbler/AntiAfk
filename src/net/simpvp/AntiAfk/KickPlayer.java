77740a9f24d421614581a11f26f8d16f9d1fb577
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-10-09 15:09:26 -0400

    Update KickPlayer.java

--- src/net/simpvp/AntiAfk/KickPlayer.java
+++ src/net/simpvp/AntiAfk/KickPlayer.java
@@ -1,9 +1,12 @@
 package net.simpvp.AntiAfk;
 
+import java.util.Iterator;
+
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class KickPlayer {
 	
@@ -11,24 +14,43 @@ public class KickPlayer {
 
 	public static void online_check() {
 		
-		if (System.currentTimeMillis() < (1 * 60 * 1000) + last_request) {
+		if (System.currentTimeMillis() < (AntiAfk.online_check_seconds * 1000) + last_request) {
 			return;
 		}
 		
 		last_request = System.currentTimeMillis();
 
+		TextComponent msg = new TextComponent("[Announcement] Please verify that you're online by moving ");
+		msg.setColor(ChatColor.AQUA);
+		
 		for (Player p : GetAfkPlayers.afkPlayers) {
-			p.sendMessage(ChatColor.RED + "You are about to be kicked for being afk. Please move.");
+			p.spigot().sendMessage(msg);
+			p.sendTitle(ChatColor.GOLD + "Afk Check", "Please verify that you're online", 10, 550, 20);
 		}
-		// Run this in 20 seconds
+		
+		/* Activate in 20 seconds */
+		final long activate_at = System.currentTimeMillis() + 20 * 1000;
 		new BukkitRunnable() {
-		     @Override
-		     public void run() {
-		 		for (Player p : GetAfkPlayers.afkPlayers) {
-					p.kickPlayer(ChatColor.RED + "Kicked for being afk in low tps.");
+			@Override
+			public void run() {
+				if (System.currentTimeMillis() < activate_at) {
+					return;
 				}
-		          cancel();
-		     }
-		}.runTaskTimer(AntiAfk.instance, 400, 1);
+
+				this.cancel();
+				new BukkitRunnable() {
+					@Override
+					public void run() {
+						Player p;
+				 		for (Iterator<?> afkListIterator = AntiAfk.instance.getServer().getOnlinePlayers().iterator(); afkListIterator.hasNext();) {
+				 			p = (Player) afkListIterator.next();
+				 			if (GetAfkPlayers.isAfk(p)) {
+				 				p.kickPlayer(ChatColor.RED + "Kicked for being afk in low tps");
+				 			}
+				 		}
+					}
+				}.runTaskLater(AntiAfk.instance, 0);
+			}
+		}.runTaskTimerAsynchronously(AntiAfk.instance, 20L, 20L);
 	}
 }
